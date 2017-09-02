211290258f84161c41ac92c464144da32dbe2371
Ashok Dey <ashokdey100@gmail.com>
2017-09-02 21:59:47 +0530

    UI with social login buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,22 +2,33 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class Header extends Component{
+
+  renderContent(){
+    switch(this.props.auth){
+      case null: 
+        return 'Waiting'
+      case false:
+        return 'Welcome'
+      default:
+        return 'Hello'
+    }
+  }
+
   render(){
     // see the state as a prop
-    console.log(this.props);
+    // console.log(this.props);
     return(
       <nav>
         <div className="nav-wrapper">
           <a href="#!" className="brand-logo" style={{marginLeft: '10px'}}>ReactTunes</a>
           <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
+            {this.renderContent()}
             <li><a href="sass.html">Login with Google</a></li>
             <li><a href="badges.html">Login  with Facebook</a></li>
             <li><a href="collapsible.html">About</a></li>
           </ul>
           <ul className="side-nav" id="mobile-demo">
-            <li><a href="sass.html">Login with Google</a></li>
-            <li><a href="badges.html">Login  with Facebook</a></li>
             <li><a href="collapsible.html">About</a></li>
           </ul>
         </div>

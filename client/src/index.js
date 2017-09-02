893056084d92ac183ad004730bf1bd3081bc63cd
Ashok Dey <ashokdey100@gmail.com>
2017-09-02 18:04:04 +0530

    removed boilerplate code, initial setup for react and redux, auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import reducers from './reducers';
+
+// create the store with dummy reducer and emptystate
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('app'));

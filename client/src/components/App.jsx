017d53652d262f30c7574315c61e4d480446b3bd
Ashok Dey <ashokdey100@gmail.com>
2017-09-02 18:41:40 +0530

    router setup and HeaderNav

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Header from './Header.jsx';
+
+const Dashboard = () => <h1> Dashboard </h1>;
+const Song = () => <h1> Song </h1>;
+const Landing = () => <h1> Landing </h1>;
+
+const App = () => {
+  return(
+    <div>
+      <Router>
+        <div>
+          <Header/>        
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/dashboard" component={Dashboard} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

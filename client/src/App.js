9233d9615c1648baad6dfeb776b50f82860fea80
Ashok Dey <ashokdey100@gmail.com>
2017-09-02 17:12:28 +0530

    proxy in react app, links testing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <a href="/auth/google">Login with Google</a> 
+          <br/>
+          <a href="/auth/facebook">Login with Facebook</a> 
         </p>
       </div>
     );

8cb6bf97866bf2c1250c825283fa351a8b6a09a6
Ashok Dey <ashokdey100@gmail.com>
2017-09-02 14:58:35 +0530

    change in callbackURL

--- settings/index.js
+++ settings/index.js
@@ -14,12 +14,12 @@ const config = {
     facebook: {
       clientID: '1444953775559638',
       clientSecret: 'eb28c2cd234775303c4eae33e0ac03ad',
-      callbackURL: '/auth/facebook/callback'
+      callbackURL: '/auth/facebook/callback/'
     },
     twitter: {
       consumerKey: '',
       consumerSecret: '',
-      callbackURL: '/auth/twitter/callback'
+      callbackURL: '/auth/twitter/callback/'
     },
     google: {
       clientID: '544201087166-h3814jgsusojjqooq2nmqqrrbdrtolfc.apps.googleusercontent.com',
